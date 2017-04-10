225b561962051091207b98d2297a3a5cc268a9a8
Kordian Ledzion <kkordikk@gmail.com>
2017-04-10 21:58:20 +0200

    InitialCommit

---
+++ PE002/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PE002
+//
+//  Created by Kordian Ledzion on 10.04.2017.
+//  Copyright © 2017 KordianLedzion. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
